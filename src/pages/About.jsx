2900fa1b2c312597ada6e0edd93e4772e9265b32
@Purnesh1226 <purnesh.kalavagunta@clouddatanetworks.com>
2024-05-23 18:57:51 +0530

    NInth Commit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,6 +3,7 @@ import Footer from "../components/Footer";
 import AboutOne from "../components/AboutOne";
 import AboutTwo from "../components/AboutTwo";
 import AboutThree from "../components/AboutThree";
+import AboutFour from "../components/AboutFour";
 
 const About = () => {
   return (
@@ -11,6 +12,7 @@ const About = () => {
       <AboutOne />
       <AboutTwo />
       <AboutThree />
+      <AboutFour />
       <Footer />
     </>
   );
