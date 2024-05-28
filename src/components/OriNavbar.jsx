2b9b24ee2941d6dbbda5138133e74900d4c2f810
@Purnesh1226 <purnesh.kalavagunta@clouddatanetworks.com>
2024-05-28 10:32:55 +0530

    Twenty One

--- src/components/OriNavbar.jsx
+++ src/components/OriNavbar.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
 import { AiOutlineClose } from "react-icons/ai";
 import { AiOutlineMenu } from "react-icons/ai";
 import Logo from "../assets/logos/FooterLogo.png";
@@ -9,9 +9,17 @@ import Logo from "../assets/logos/FooterLogo.png";
 // import { FaYoutube } from "react-icons/fa";
 // import { IoIosMail } from "react-icons/io";
 // import { IoMdPhonePortrait } from "react-icons/io";
+import { FiChevronDown } from "react-icons/fi";
+import { LuGraduationCap } from "react-icons/lu";
+import { VscSymbolMethod } from "react-icons/vsc";
+import { FaIndustry } from "react-icons/fa";
+import { GrResources } from "react-icons/gr";
+import { motion } from "framer-motion";
+// import { IconType } from "react-icons";
 
 const OriNavbar = () => {
   const [nav, setNav] = useState(false);
+  const [open, setOpen] = useState(false);
 
   const handleNav = () => {
     setNav(!nav);
@@ -71,10 +79,58 @@ const OriNavbar = () => {
                   <a href="/solutions">Solutions</a>
                   <div className="bg-colorFour h-[2px] w-0 group-hover:w-full transition-all duration-500"></div>
                 </li>
-                <li className="group cursor-pointer text-colorThree font-medium text-base tracking-wider transition duration-1000 ease-in-out">
-                  <a href="/careers">Careers</a>
-                  <div className="bg-colorFour h-[2px] w-0 group-hover:w-full transition-all duration-500"></div>
-                </li>
+                <motion.div
+                  animate={open ? "open" : "closed"}
+                  className="relative"
+                >
+                  <button
+                    onClick={() => setOpen((pv) => !pv)}
+                    className="flex items-center gap-2 rounded-md "
+                  >
+                    <span className="font-medium text-base text-colorThree">
+                      Careers
+                    </span>
+                    <motion.span variants={iconVariants}>
+                      <FiChevronDown />
+                    </motion.span>
+                  </button>
+
+                  <motion.ul
+                    initial={wrapperVariants.closed}
+                    variants={wrapperVariants}
+                    style={{ originY: "top", translateX: "-50%" }}
+                    className="flex flex-col gap-2 p-2 rounded-lg bg-white shadow-xl absolute top-[120%] left-[50%] w-48 overflow-hidden"
+                  >
+                    <a href="/careers">
+                      <Option
+                        setOpen={setOpen}
+                        Icon={LuGraduationCap}
+                        text="Career Support"
+                      />
+                    </a>
+                    <a href="/contact">
+                      <Option
+                        setOpen={setOpen}
+                        Icon={VscSymbolMethod}
+                        text="Our Approach"
+                      />
+                    </a>
+                    <a href="/careers">
+                      <Option
+                        setOpen={setOpen}
+                        Icon={FaIndustry}
+                        text="Our Impact"
+                      />
+                    </a>
+                    <a href="/contact">
+                      <Option
+                        setOpen={setOpen}
+                        Icon={GrResources}
+                        text="Resources"
+                      />
+                    </a>
+                  </motion.ul>
+                </motion.div>
               </ul>
             </div>
 
@@ -135,4 +191,63 @@ const OriNavbar = () => {
   );
 };
 
+const Option = ({ text, Icon, setOpen }) => {
+  return (
+    <motion.li
+      variants={itemVariants}
+      onClick={() => setOpen(false)}
+      className="flex items-center gap-2 w-full p-2 text-xs font-medium whitespace-nowrap rounded-md hover:bg-indigo-100 text-slate-700 hover:text-indigo-500 transition-colors cursor-pointer"
+    >
+      <motion.span variants={actionIconVariants}>
+        <Icon />
+      </motion.span>
+      <span>{text}</span>
+    </motion.li>
+  );
+};
+
 export default OriNavbar;
+
+const wrapperVariants = {
+  open: {
+    scaleY: 1,
+    transition: {
+      when: "beforeChildren",
+      staggerChildren: 0.1,
+    },
+  },
+  closed: {
+    scaleY: 0,
+    transition: {
+      when: "afterChildren",
+      staggerChildren: 0.1,
+    },
+  },
+};
+
+const iconVariants = {
+  open: { rotate: 180 },
+  closed: { rotate: 0 },
+};
+
+const itemVariants = {
+  open: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      when: "beforeChildren",
+    },
+  },
+  closed: {
+    opacity: 0,
+    y: -15,
+    transition: {
+      when: "afterChildren",
+    },
+  },
+};
+
+const actionIconVariants = {
+  open: { scale: 1, y: 0 },
+  closed: { scale: 0, y: -7 },
+};
