339be1e75d28efaef7dd50b313eedcc7241b406e
@Purnesh1226 <purnesh.kalavagunta@clouddatanetworks.com>
2024-06-02 00:11:44 +0530

    Twenty Four Seven

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,11 +7,31 @@ import "swiper/css/autoplay";
 import "swiper/css/controller";
 
 const Hero = () => {
-  const images = [
-    "https://images.pexels.com/photos/1139556/pexels-photo-1139556.jpeg?auto=compress&cs=tinysrgb&w=1200",
-    "https://images.pexels.com/photos/290595/pexels-photo-290595.jpeg?auto=compress&cs=tinysrgb&w=1200",
-    "https://images.pexels.com/photos/534174/pexels-photo-534174.jpeg?auto=compress&cs=tinysrgb&w=1200",
-    "https://images.pexels.com/photos/233698/pexels-photo-233698.jpeg?auto=compress&cs=tinysrgb&w=1200",
+  const data = [
+    {
+      image:
+        "https://images.pexels.com/photos/1139556/pexels-photo-1139556.jpeg?auto=compress&cs=tinysrgb&w=1200",
+      heading: "IT Software Solution & Support.",
+      button: "Collaborate with us",
+    },
+    {
+      image:
+        "https://images.pexels.com/photos/1139556/pexels-photo-1139556.jpeg?auto=compress&cs=tinysrgb&w=1200",
+      heading: "Vahas Technolgies Support.",
+      button: "Explore with us",
+    },
+    {
+      image:
+        "https://images.pexels.com/photos/290595/pexels-photo-290595.jpeg?auto=compress&cs=tinysrgb&w=1200",
+      heading: "Solid battle tested Infrastructure.",
+      button: "Make a better future",
+    },
+    {
+      image:
+        "https://images.pexels.com/photos/534174/pexels-photo-534174.jpeg?auto=compress&cs=tinysrgb&w=1200",
+      heading: "Better User Interface Structure.",
+      button: "Journey Starts here",
+    },
   ];
 
   return (
@@ -22,25 +42,25 @@ const Hero = () => {
         slidesPerView={1}
         autoplay
       >
-        {images.map((img, index) => (
+        {data.map((img, index) => (
           <SwiperSlide key={index}>
             <div className="relative">
               <img
-                src={img}
+                src={img.image}
                 alt={`slide-${index + 1}`}
                 className="w-full h-[500px] object-cover"
               />
               <div className="absolute inset-0 bg-gray-700 opacity-40 rounded-md"></div>
               <div className="absolute inset-0 flex flex-col items-center justify-center">
                 <h2 className="text-white text-center text-3xl font-bold">
-                  Upgrade with technological support.
+                  {img.heading}
                 </h2>
                 <div className="mt-8">
                   <a
                     href="/contact"
-                    className="py-2 px-10 border-black border-2 rounded-3xl font-semibold hover:bg-black hover:text-white transition ease-in-out duration-1000"
+                    className="py-2 px-10 border-black border-2 rounded-3xl font-semibold bg-black text-white transition ease-in-out duration-1000"
                   >
-                    Collaborate with us &#8594;
+                    {img.button} &#8594;
                   </a>
                 </div>
               </div>
