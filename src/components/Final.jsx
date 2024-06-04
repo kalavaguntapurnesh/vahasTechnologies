572ec7cb29a64be5150d9a0674f5fdbe17836e6a
@Purnesh1226 <purnesh.kalavagunta@clouddatanetworks.com>
2024-06-04 13:00:19 +0530

    Buttons

--- src/components/Final.jsx
+++ src/components/Final.jsx
@@ -28,10 +28,31 @@ const Final = () => {
                     <div className="mt-4 mb-4">
                       <div className="flex flex-row md:justify-start justify-center">
                         <a
-                          href="/contact"
-                          className="py-2 px-8 border-black border-2 rounded-3xl font-semibold hover:bg-black hover:text-white transition ease-in-out duration-1000"
+                          href="/careers"
+                          className="relative inline-flex items-center justify-center py-2 px-10 overflow-hidden font-semibold text-companyColor transition duration-1000 ease-out border-2 border-companyColor rounded-3xl group"
                         >
-                          Contact Us
+                          <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-companyColor group-hover:translate-x-0 ease-in-out">
+                            <svg
+                              className="w-6 h-6"
+                              fill="none"
+                              stroke="currentColor"
+                              viewBox="0 0 24 24"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M14 5l7 7m0 0l-7 7m7-7H3"
+                              ></path>
+                            </svg>
+                          </span>
+                          <span className="absolute flex items-center justify-center w-full h-full text-companyColor transition-all duration-1000 transform group-hover:translate-x-full ease-in-out">
+                            Contact us &#8594;
+                          </span>
+                          <span className="relative invisible">
+                            Button Text
+                          </span>
                         </a>
                       </div>
                     </div>
