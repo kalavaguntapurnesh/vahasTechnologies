082d0aa2a6d27823de6fea73cf2bcad9d07a4894
@Purnesh1226 <purnesh.kalavagunta@clouddatanetworks.com>
2024-06-06 15:38:32 +0530

    Spinners

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,3 @@
-// import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import Footer from "../components/Footer";
 import Working from "../components/Working";
@@ -7,39 +6,19 @@ import Record from "../components/Record";
 import OriNavbar from "../components/OriNavbar";
 import Benefits from "../components/Benefits";
 import ScrollToTop from "../components/ScrollToTop";
-// import PropagateLoader from "react-spinners";
-import { useState } from "react";
 
 const Home = () => {
-  const [isLoading, setIsLoading] = useState(true);
-
-  setTimeout(() => {
-    setIsLoading(false);
-  }, 1000);
-
   return (
-    <div>
-      {isLoading ? (
-        <div className="flex flex-col items-center justify-center min-h-screen">
-          <div className="flex space-x-2">
-            <div className="h-4 w-4 rounded-full bg-companyColor animate-bounce"></div>
-            <div className="h-4 w-4 rounded-full bg-companyColor animate-bounce2"></div>
-            <div className="h-4 w-4 rounded-full bg-companyColor animate-bounce"></div>
-          </div>
-        </div>
-      ) : (
-        <div>
-          <OriNavbar />
-          <Hero />
-          <Record />
-          <ScrollToTop />
-          <Working />
-          <Benefits />
-          <Final />
-          <Footer />
-        </div>
-      )}
-    </div>
+    <>
+      <OriNavbar />
+      <Hero />
+      <Record />
+      <ScrollToTop />
+      <Working />
+      <Benefits />
+      <Final />
+      <Footer />
+    </>
   );
 };
 
