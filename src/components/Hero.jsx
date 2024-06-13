534309488c16a1f39ba6b432c7d2dc3ef1e0dbcf
@Purnesh1226 <purnesh.kalavagunta@clouddatanetworks.com>
2024-06-13 11:23:28 +0530

    Object Placement

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -48,7 +48,7 @@ const Hero = () => {
               <img
                 src={img.image}
                 alt={`slide-${index + 1}`}
-                className="w-full h-[500px] object-cover"
+                className="w-full h-[500px] lg:object-cover object-center"
               />
               <div className="absolute inset-0 bg-gray-700 opacity-40 rounded-md"></div>
               <div className="absolute inset-0 flex flex-col items-center justify-center">
